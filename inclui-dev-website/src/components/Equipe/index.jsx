1ab18bf6f1e08bd143590862e893a255d29c0260
HenryModesto <modestohenry48@gmail.com>
2024-06-02 14:40:59 -0300

    FETCH(EQUIPE RESPONSE)

--- inclui-dev-website/src/components/Equipe/index.jsx
+++ inclui-dev-website/src/components/Equipe/index.jsx
@@ -14,84 +14,87 @@ export default function Equipe() {
     return (
         <section id='Equipe' className='Equipe'>
             <div className='blocos-Equipe'><img className='CenterBlocos' src={blocosCenter} /></div>
-            
             <h2 className='title-equipe'>EQUIPE</h2>
-                <div className="members-one">
-                
-            <EquipeCard
-            
-                foto = {KaiqueFoto} 
-                nome = "Kaique Sousa"
-                funcao = "Product Owner & FullStack" 
-                github = "https://github.com/kaiquesouzasantos"
-                linkedin = "https://www.linkedin.com/in/kaique-souza-santos/"
-                instagram = "https://www.instagram.com/kaiquesouzasantoss/" />
+            <div className="members-one">
+                <EquipeCard
+                    foto={KaiqueFoto} 
+                    nome="KAIQUE SOUSA"
+                    funcao="Product Owner & FullStack"
+                    github="https://github.com/kaiquesouzasantos"
+                    linkedin="https://www.linkedin.com/in/kaique-souza-santos/"
+                    instagram="https://www.instagram.com/kaiquesouzasantoss/"
+                    cor="#FF725E" 
+                 
+                />
 
+                <EquipeCard
+                    foto={HenryFoto} 
+                    nome="HENRY MODESTO"
+                    funcao="Scrum Master, UX/UI, FullStack"
+                    github="https://github.com/HenryModesto"
+                    linkedin="https://www.linkedin.com/in/henrymodesto/"
+                    instagram="https://www.instagram.com/henry_modesto05/"
+                    cor="#4D8F9A" 
+                />
 
-            <EquipeCard 
-                foto = {HenryFoto} 
-                nome = "Henry Modesto"
-                funcao = "Scrum Master & FullStack"
-                github = "https://github.com/HenryModesto"
-                linkedin = "https://www.linkedin.com/in/henrymodesto/" 
-                instagram = "https://www.instagram.com/henry_modesto05/" />
-                
+                <EquipeCard
+                    foto={KaykFoto} 
+                    nome="KAYK GOMES"
+                    funcao="Financeiro & Back-End"
+                    github="https://github.com/Kayk-Dev"
+                    linkedin="https://www.linkedin.com/in/kaykmg/"
+                    instagram="https://www.instagram.com/kaykmessiass/"
+                    cor="#63AB72" 
+                />
 
-            <EquipeCard 
-                foto = {KaykFoto} 
-                nome = "Kayk Gomes"
-                funcao = "Financeiro & Back-End" 
-                github = "https://github.com/Kayk-Dev"
-                linkedin = "https://www.linkedin.com/in/kaykmg/" 
-                instagram = "https://www.instagram.com/kaykmessiass/" />
-
-
-            <EquipeCard 
-                foto = {AlineFoto} 
-                nome = "Aline Aguiar"
-                funcao = "Marketing - UX/UI & Front-End"
-                github = "https://github.com/eulineaguiar"
-                linkedin = "https://www.linkedin.com/in/alineaguiarsilva/" 
-                instagram = "https://www.instagram.com/eulineaguiar/" />
-
+                <EquipeCard
+                    foto={AlineFoto} 
+                    nome="ALINE AGUIAR"
+                    funcao="Marketing - UX/UI & Front-End"
+                    github="https://github.com/eulineaguiar"
+                    linkedin="https://www.linkedin.com/in/alineaguiarsilva/"
+                    instagram="https://www.instagram.com/eulineaguiar/"
+                    cor="#C45BAA" 
+                />
             </div>
-            
             <div className="members-two">
-            <EquipeCard 
-                foto = {AdrieleFoto} 
-                nome = "Adriele Lima"
-                funcao = "Marketing & Back-End" 
-                github = "#"
-                linkedin = "https://www.linkedin.com/in/adrielels/" 
-                instagram = "https://www.instagram.com/adriele.7/" />
-
-
-            <EquipeCard 
-                foto = {IgorFoto} 
-                nome = "Igor Silva"
-                funcao = "Designer & Front-End" 
-                github = "https://github.com/IgorOdSilva"
-                linkedin = "https://www.linkedin.com/in/igorodsilva/"
-                instagram = "https://www.instagram.com/igor_silva_150/" />
-
-
-            <EquipeCard 
-                foto = {LeonardoFoto}
-                nome = "Leonardo Duarte"
-                funcao = "Designer & Front-End" 
-                github = "https://github.com/leoduarte14"
-                linkedin = "https://www.linkedin.com/in/leonardo-duarte-pereira-a4705b255/"
-                instagram = "https://www.instagram.com/duart.yn/" />
-
-           
-            <EquipeCard 
-                foto = {KaiqueFoto}
-                nome = "Edson Fernandez"
-                funcao = "Mentor - Gerente de TI - PAM" 
-                linkedin = "https://www.linkedin.com/in/edsonfmarques/"
-                instagram = "https://www.instagram.com/edsonfernandesmarques/" />
-
+                <EquipeCard
+                    foto={AdrieleFoto} 
+                    nome="ADRIELE LIMA"
+                    funcao="Marketing & Back-End"
+                    github="#"
+                    linkedin="https://www.linkedin.com/in/adrielels/"
+                    instagram="https://www.instagram.com/adriele.7/"
+                    cor="#FFAA1D" 
+                />
+                
+                <EquipeCard
+                    foto={IgorFoto} 
+                    nome="IGOR SILVA"
+                    funcao="Designer & Front-End"
+                    github="https://github.com/IgorOdSilva"
+                    linkedin="https://www.linkedin.com/in/igorodsilva/"
+                    instagram="https://www.instagram.com/igor_silva_150/"
+                    cor="#C45BAA" 
+                />
+                <EquipeCard
+                    foto={LeonardoFoto}
+                    nome="LEONARDO DUARTE"
+                    funcao="Designer & Front-End"
+                    github="https://github.com/leoduarte14"
+                    linkedin="https://www.linkedin.com/in/leonardo-duarte-pereira-a4705b255/"
+                    instagram="https://www.instagram.com/duart.yn/"
+                    cor="#7DCEA0" 
+                />
+                <EquipeCard
+                    foto={KaiqueFoto}
+                    nome="EDSON FERNANDEZ"
+                    funcao="Mentor - Gerente de TI - PAM"
+                    linkedin="https://www.linkedin.com/in/edsonfmarques/"
+                    instagram="https://www.instagram.com/edsonfernandesmarques/"
+                    cor="#FFA07A" 
+                />
             </div>
-        </section>  
+        </section>
     )
 }
--- inclui-dev-website/src/components/EquipeCard/index.jsx
+++ inclui-dev-website/src/components/EquipeCard/index.jsx
@@ -4,26 +4,21 @@ import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-
 
 export default function EquipeCard(props) {
     return (
-        <article className="equipe-card">
-            <div className='container-img-card'>
-                <img src={props.foto} alt={`${props.nome} foto`} />
+        <div className="EquipeCard">
+            <div className='container-img-card' style={{ backgroundColor: props.cor }}>
+                <img src={props.foto} />
             </div>
-
             <div className='card-below'>
                 <p>{props.nome}</p>
                 <p>{props.funcao}</p>
+                <p>{props.description}</p>
                 <div className="redes-sociais">
-                    <a href={props.github} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon color={"#FF725E"} id='github-icon' icon={faGithub} className='icons' />
-                    </a>
-                    <a href={props.linkedin} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon color={"#4D8F9A"} id='linkedin-icon' icon={faLinkedin} className='icons'/>
-                    </a>
-                    <a href={props.instagram} target="_blank" rel="noopener noreferrer">
-                        <FontAwesomeIcon color={"#63AB72"} id='instagram-icon' icon={faInstagram} className='icons'/>
-                    </a>
+                    <a href={props.github} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon color={"#FF725E"} id='githubIcon' icon={faGithub} className='icons' /></a>
+                    <a href={props.linkedin} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon color={"#4D8F9A"} id='linkedinIcon' icon={faLinkedin} className='icons'/></a>
+                    <a href={props.instagram} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon color={"#63AB72"} id='instagramIcon' icon={faInstagram} className='icons'/></a>
                 </div>
+               
             </div>
-        </article>
+        </div>
     )
 }
