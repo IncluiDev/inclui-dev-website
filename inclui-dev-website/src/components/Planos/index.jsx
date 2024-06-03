ef42f050879698174af935335a8bcf641e4c09da
KaiqueSouzaSantos <kaiquesouzasantos905@gmail.com>
2024-06-03 00:08:37 -0300

    REFACT(alinhamento de planos card)

--- inclui-dev-website/src/components/Planos/index.jsx
+++ inclui-dev-website/src/components/Planos/index.jsx
@@ -48,6 +48,7 @@ export default function Planos() {
                 <h3>Nossos Planos</h3>
                 <p>Escolha o plano que melhor se adapta às suas necessidades e <br></br>aproveite ao máximo nossos serviços.</p>
             </div>
+            
             <div className="cartoes">
                 {planosData.map((plano, index) => (
                     <PlanosCard key={index} title={plano.title} details={plano.details} image={plano.image} />
