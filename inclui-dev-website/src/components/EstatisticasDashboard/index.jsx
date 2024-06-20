79e1960520c8edf8f6a6ef6a95922a709c7f2380
HenryModesto <modestohenry48@gmail.com>
2024-06-19 23:50:20 -0300

    FETCH(Ajustes Dashboard)

--- inclui-dev-website/src/components/EstatisticasDashboard/index.jsx
+++ inclui-dev-website/src/components/EstatisticasDashboard/index.jsx
@@ -1,26 +1,35 @@
-import React from 'react'
-import './style.css'
-import estatisticas from '../../assets/estatisticas.png'
-import planoEstatisticas from '../../assets/planoEstatisticas.png'
+import React from 'react';
+import './style.css';
+import estatisticas from '../../assets/estatisticas.png';
+import premiumImg from '../../assets/premiumImg.png';
 
 const EstatisticasDashboard = () => {
   return (
     <>
-    <section className='estatisticas-dashboard-container'>
-        <h2>Suas estatisticas</h2>
+      <section className='estatisticas-dashboard-container'>
+        <h2>Suas estatísticas</h2>
         <nav className='estatisticas-nav'>
-            <ul className='estatisticas-nav-lista'>
-                <li>Horas de Aprendizado</li>
-                <li>Meus cursos</li>
-                <button className='mes-nav'>Mês</button>
-
-            </ul>
-            <img src={estatisticas} alt=""  className='estatisticas'/>
-            <img src={planoEstatisticas} alt=""  className='planoEstatisticas'/>
+          <ul className='estatisticas-nav-lista'>
+            <li>Horas de Aprendizado</li>
+            <li>Meus cursos</li>
+            <button className='mes-nav'>Mês</button>
+          </ul>
+          <div className='estatisticas-imagens'>
+            <img src={estatisticas} alt="Estatísticas" className='estatisticas' />
+            </div>
+            <div className="card-premium">
+            
+              <div className="card-content">
+             
+               <img src={premiumImg} alt="Plano de Estatísticas" className='premiumImg' />
+               <button className='button-dashboard'>Go Premium</button>
+              </div>
+            </div>
+          
         </nav>
-    </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default EstatisticasDashboard
+export default EstatisticasDashboard;
--- inclui-dev-website/src/components/TopoDashboard/index.jsx
+++ inclui-dev-website/src/components/TopoDashboard/index.jsx
@@ -1,39 +1,42 @@
-import "./style.css"
+import React from 'react'
+import './style.css'
 import pessoaDashboard from '../../assets/pessoaDashboard.png'
 import userProfile from '../../assets/user-profile.png'
 import { MdOutlineNotificationsActive } from "react-icons/md";
+import Bolhas from '../../assets/bolhas.png'
 
 const TopoDashboard = () => {
   return (
     <header className='topo-dashboard-container'>
-        <div className='topo-dashboard'>
-            <div className='saudacao-user-container'>
-                <div className='saudacao-user-texto'>
-                    <h1>Olá, Dev!</h1>
-                    <p>Feliz em te ver novamente!</p>
-                </div>
-
-                <img src={pessoaDashboard} alt="Pessoa no Dashboard" className='pessoaDashboard'/>
-            </div>
-            
-            <aside className='info-topo-container'>
-                <div className='info-topo'>
-                    <input type="search" placeholder='Pesquisar Curso' className='pesquisa-curso' aria-label="Pesquisar Curso"/>
-                    <MdOutlineNotificationsActive className="notification-icon"/>
-                    <img src={userProfile} alt="Perfil do Usuário" className='user-profile'/>
-                </div>
-                <section className='cursos-dashboard'>
-                    <div className='curso-completo'>
-                        <h2>04</h2>
-                        <p>Cursos completos</p>
-                    </div>
-                    <div className='curso-progresso'>
-                        <h2>05</h2>
-                        <p>Cursos em progresso</p>
-                    </div>
-                </section>
-            </aside>
+      <div className='topo-dashboard'>
+        <div className='saudacao-user-container'>
+          <div className='bolhas-background'></div>
+          <div className='saudacao-user-texto'>
+            <h1>Olá, Dev!</h1>
+            <p>Feliz em te ver novamente!</p>
+          </div>
+          <div className="pessoaImg">
+            <img src={pessoaDashboard} alt="Pessoa no Dashboard" className='pessoaDashboard'/>
+          </div>
         </div>
+        <aside className='info-topo-container'>
+          <div className='info-topo'>
+            <input type="search" placeholder='Pesquisar Curso' className='pesquisa-curso' aria-label="Pesquisar Curso"/>
+            <MdOutlineNotificationsActive className="notification-icon"/>
+            <img src={userProfile} alt="Perfil do Usuário" className='user-profile'/>
+          </div>
+          <section className='cursos-dashboard'>
+            <div className='curso-completo'>
+              <h2>04</h2>
+              <p>Cursos completos</p>
+            </div>
+            <div className='curso-progresso'>
+              <h2>05</h2>
+              <p>Cursos em progresso</p>
+            </div>
+          </section>
+        </aside>
+      </div>
     </header>
   )
 }
