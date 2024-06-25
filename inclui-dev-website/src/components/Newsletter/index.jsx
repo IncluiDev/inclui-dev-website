3e0f3bb94c8189cf24a7fe24f2b8c402a608b0ce
KaiqueSouzaSantos <kaiquesouzasantos905@gmail.com>
2024-06-25 11:10:24 -0300

    REFACT(newsletter)

--- inclui-dev-website/src/components/Newsletter/index.jsx
+++ inclui-dev-website/src/components/Newsletter/index.jsx
@@ -15,7 +15,7 @@ export default function Newsletter() {
           email: event.target.email.value,
         };
     
-        fetch("http://localhost:8085/usuario", {
+        fetch("http://inclui-dev-email-api-production.up.railway.app/usuario", {
           method: "POST",
           headers: {
             'Accept': 'application/json',
